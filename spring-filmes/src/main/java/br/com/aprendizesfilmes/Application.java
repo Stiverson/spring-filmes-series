92f022b0941332a6592b49f1e6d5b550429678b7
smachado <stiverson.machado@gmail.com>
2021-10-24 20:49:53 -0300

    adição de segundo filme na lista do stive

--- spring-filmes/src/main/java/br/com/aprendizesfilmes/Application.java
+++ spring-filmes/src/main/java/br/com/aprendizesfilmes/Application.java
@@ -18,14 +18,25 @@ public class Application {
 		duna.setDiretor("Denis Vileneve");
 		duna.setDuracao("2H,28");
 		duna.setData_lancamento("21/10/2021");
+		duna.setElenco("");
 		duna.setStreaming("HBOMAX");
 
+		Filmes spiderverse = new Filmes();
+		spiderverse.setId(2);
+		spiderverse.setTitulo("Homem Aranha no Aranha verso");
+		spiderverse.setDiretor("");
+		spiderverse.setDuracao("");
+		spiderverse.setData_lancamento("");
+		spiderverse.setElenco("");
+		spiderverse.setStreaming("Netflix");
+
 		Filmes jogosvorazes = new Filmes();
-		jogosvorazes.setId(2);
+		jogosvorazes.setId(3);
 		jogosvorazes.setTitulo("Jogos Vorazes");
 		jogosvorazes.setDiretor("Gary Ross");
 		jogosvorazes.setDuracao("2H,22");
 		jogosvorazes.setData_lancamento("22/03/2012");
+		jogosvorazes.setElenco("");
 		jogosvorazes.setStreaming("Netflix e Amazon Prime");
 
 		Gson gson = new Gson();
